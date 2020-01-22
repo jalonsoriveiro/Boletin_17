3f26088d5795b644f60de7dc4f09446571722e9c
Jose Alonso <jalonsoriveiro@danielcastelao.org>
2020-01-22 10:59:39 +0100

    first commit

---
+++ boletin_17/Avestruza.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_17;
+
+/**
+ *
+ * @author jalonsoriveiro
+ */
+public class Avestruza extends Aves implements IPodeCamiñar{
+
+    @Override
+    public void caminar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ boletin_17/Gato.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_17;
+
+/**
+ *
+ * @author jalonsoriveiro
+ */
+public class Gato extends Felino implements IPodeCamiñar,IPodeNadar{
+
+    @Override
+    public void caminar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void nadar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
